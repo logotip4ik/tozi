a94d8df3aeac02649e66bc62417da838656d3408
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2025-12-27 23:27:03 +0200

    feat: compute info hash

--- src/root.zig
+++ src/root.zig
@@ -9,20 +9,48 @@ const Torrent = struct {
     created_by: ?[]const u8,
     info: std.StringHashMapUnmanaged(Value) = .empty,
 
+    /// reader must be recreated to seek from start of the file
+    pub fn computeInfoHash(self: Torrent, reader: *std.Io.Reader) ![std.crypto.hash.Sha1.digest_length]u8 {
+        const info = self.value.inner.dict.get("info") orelse unreachable;
+
+        _ = try reader.discardShort(info.start);
+
+        var hash: std.crypto.hash.Sha1 = .init(.{});
+
+        var read: usize = 0;
+        while (true) {
+            const readSize = @min(std.crypto.hash.Sha1.block_length, info.len - read);
+            const chunk = try reader.take(readSize);
+
+            hash.update(chunk);
+            read += readSize;
+
+            if (read == info.len) {
+                break;
+            }
+        }
+
+        return hash.finalResult();
+    }
+
     pub fn deinit(self: *Torrent, alloc: std.mem.Allocator) void {
         self.value.deinit(alloc);
         if (self.announceList) |list| alloc.free(list);
     }
 };
 
-const Value = union(enum) {
-    int: usize,
-    string: []const u8,
-    list: std.array_list.Aligned(Value, null),
-    dict: std.StringHashMapUnmanaged(Value),
+const Value = struct {
+    inner: union(enum) {
+        int: usize,
+        string: []const u8,
+        list: std.array_list.Aligned(Value, null),
+        dict: std.StringHashMapUnmanaged(Value),
+    },
+    start: usize,
+    len: usize,
 
     fn dumpWithOptions(self: Value, writer: *std.Io.Writer, indent: u8) void {
-        switch (self) {
+        switch (self.inner) {
             .int => |int| {
                 for (0..indent) |_| writer.writeByte(' ') catch unreachable;
                 writer.print("{d}\n", .{int}) catch unreachable;
@@ -58,14 +86,14 @@ const Value = union(enum) {
     }
 
     fn deinit(self: *Value, alloc: std.mem.Allocator) void {
-        switch (self.*) {
-            Value.int => {},
-            Value.string => |string| alloc.free(string),
-            Value.list => |*list| {
+        switch (self.inner) {
+            .int => {},
+            .string => |string| alloc.free(string),
+            .list => |*list| {
                 for (list.items) |*item| item.deinit(alloc);
                 list.deinit(alloc);
             },
-            Value.dict => |*dict| {
+            .dict => |*dict| {
                 var iter = dict.iterator();
                 while (iter.next()) |*entry| {
                     alloc.free(entry.key_ptr.*);
@@ -77,17 +105,21 @@ const Value = union(enum) {
     }
 };
 
-fn parseString(alloc: std.mem.Allocator, reader: *std.Io.Reader) ![]const u8 {
+fn parseString(alloc: std.mem.Allocator, reader: *std.Io.Reader) !struct { []const u8, usize } {
     const keyLengthString = try reader.takeDelimiter(':') orelse {
         std.log.err("expected key length to be present", .{});
         return error.MissingColon;
     };
 
     const keyLength = try std.fmt.parseUnsigned(usize, keyLengthString, 10);
-    return try reader.readAlloc(alloc, keyLength);
+
+    const key = try reader.readAlloc(alloc, keyLength);
+    const len = keyLengthString.len + 1 + keyLength;
+
+    return .{ key, len };
 }
 
-pub fn parseValue(alloc: std.mem.Allocator, reader: *std.Io.Reader) !Value {
+pub fn parseValue(alloc: std.mem.Allocator, reader: *std.Io.Reader, start: usize) !Value {
     const byte = try reader.peekByte();
 
     switch (byte) {
@@ -104,31 +136,36 @@ pub fn parseValue(alloc: std.mem.Allocator, reader: *std.Io.Reader) !Value {
                 ret.deinit(alloc);
             }
 
+            // one for leading `d`
+            var len: usize = 1;
+
             while (reader.peekByte() catch null) |next| {
                 if (next == 'e') {
                     _ = try reader.discardShort(1);
                     break;
                 }
 
-                const key = try parseString(alloc, reader);
+                const key, const keyLen = try parseString(alloc, reader);
                 errdefer alloc.free(key);
 
-                // std.debug.print("key: {s}\n", .{key});
+                len += keyLen;
 
-                var value = try parseValue(alloc, reader);
+                var value = try parseValue(alloc, reader, len);
                 errdefer value.deinit(alloc);
 
-                // switch (value) {
-                //     .string => |string| std.debug.print("value string: {s}\n", .{string}),
-                //     .int => |int| std.debug.print("value int: {d}\n", .{int}),
-                //     .list => |list| std.debug.print("value list: {d}\n", .{list.items.len}),
-                //     .dict => |dict| std.debug.print("value dict: {d}\n", .{dict.size}),
-                // }
+                len += value.len;
 
                 try ret.put(alloc, key, value);
             }
 
-            return .{ .dict = ret };
+            // and one for ending `e`
+            len += 1;
+
+            return Value{
+                .inner = .{ .dict = ret },
+                .start = start,
+                .len = len,
+            };
         },
 
         'l' => {
@@ -140,30 +177,39 @@ pub fn parseValue(alloc: std.mem.Allocator, reader: *std.Io.Reader) !Value {
                 ret.deinit(alloc);
             }
 
+            // one for leading `l`
+            var len: usize = 1;
+
             while (reader.peekByte() catch null) |next| {
                 if (next == 'e') {
                     _ = try reader.discardShort(1);
                     break;
                 }
 
-                var value = try parseValue(alloc, reader);
+                var value = try parseValue(alloc, reader, len);
                 errdefer value.deinit(alloc);
 
-                // switch (value) {
-                //     .string => |string| std.debug.print("parsed list string: {s}\n", .{string}),
-                //     .int => |int| std.debug.print("parsed list int: {d}\n", .{int}),
-                //     .list => |list| std.debug.print("parsed list list: {d}\n", .{list.items.len}),
-                //     .dict => |dict| std.debug.print("parsed list dict: {d}\n", .{dict.size}),
-                // }
-
                 try ret.append(alloc, value);
+                len += value.len;
             }
 
-            return .{ .list = ret };
+            // and one for ending `e`
+            len += 1;
+
+            return Value{
+                .inner = .{ .list = ret },
+                .start = start,
+                .len = len,
+            };
         },
 
         '0'...'9' => {
-            return .{ .string = try parseString(alloc, reader) };
+            const string, const len = try parseString(alloc, reader);
+            return Value{
+                .inner = .{ .string = string },
+                .start = start,
+                .len = len,
+            };
         },
 
         'i' => {
@@ -172,8 +218,13 @@ pub fn parseValue(alloc: std.mem.Allocator, reader: *std.Io.Reader) !Value {
             const numString = try reader.takeDelimiter('e') orelse return error.MissingNumEndMark;
 
             const int = try std.fmt.parseInt(usize, numString, 10);
+            const len = 1 + numString.len + 1;
 
-            return .{ .int = int };
+            return Value{
+                .inner = .{ .int = int },
+                .start = start,
+                .len = len,
+            };
         },
 
         else => {
@@ -190,11 +241,11 @@ test "parseValue string" {
         \\8:announce
     );
 
-    var value = try parseValue(std.testing.allocator, &reader);
+    var value = try parseValue(std.testing.allocator, &reader, 0);
     defer value.deinit(std.testing.allocator);
 
-    switch (value) {
-        .string => try std.testing.expectEqualStrings("announce", value.string),
+    switch (value.inner) {
+        .string => |string| try std.testing.expectEqualStrings("announce", string),
         else => try std.testing.expect(false),
     }
 }
@@ -204,11 +255,11 @@ test "parseValue digit" {
         \\i32e
     );
 
-    var value = try parseValue(std.testing.allocator, &reader);
+    var value = try parseValue(std.testing.allocator, &reader, 0);
     defer value.deinit(std.testing.allocator);
 
-    switch (value) {
-        .int => try std.testing.expectEqual(32, value.int),
+    switch (value.inner) {
+        .int => |int| try std.testing.expectEqual(32, int),
         else => try std.testing.expect(false),
     }
 }
@@ -218,15 +269,15 @@ test "parseValue list" {
         \\l4:http4:http4:httpe
     );
 
-    var value = try parseValue(std.testing.allocator, &reader);
+    var value = try parseValue(std.testing.allocator, &reader, 0);
     defer value.deinit(std.testing.allocator);
 
-    switch (value) {
+    switch (value.inner) {
         .list => |list| {
             try std.testing.expectEqual(3, list.items.len);
-            try std.testing.expectEqualStrings("http", list.items[0].string);
-            try std.testing.expectEqualStrings("http", list.items[1].string);
-            try std.testing.expectEqualStrings("http", list.items[2].string);
+            try std.testing.expectEqualStrings("http", list.items[0].inner.string);
+            try std.testing.expectEqualStrings("http", list.items[1].inner.string);
+            try std.testing.expectEqualStrings("http", list.items[2].inner.string);
         },
         else => try std.testing.expect(false),
     }
@@ -237,18 +288,18 @@ test "parseValue dict" {
         \\d4:httpl4:httpee
     );
 
-    var value = try parseValue(std.testing.allocator, &reader);
+    var value = try parseValue(std.testing.allocator, &reader, 0);
     defer value.deinit(std.testing.allocator);
 
-    switch (value) {
+    switch (value.inner) {
         .dict => |dict| {
             try std.testing.expectEqual(1, dict.size);
 
             const v = dict.get("http") orelse unreachable;
-            switch (v) {
+            switch (v.inner) {
                 .list => |l| {
                     try std.testing.expectEqual(1, l.items.len);
-                    try std.testing.expectEqualStrings("http", l.items[0].string);
+                    try std.testing.expectEqualStrings("http", l.items[0].inner.string);
                 },
                 else => try std.testing.expect(false),
             }
@@ -258,27 +309,27 @@ test "parseValue dict" {
 }
 
 pub fn parseTorrent(alloc: std.mem.Allocator, reader: *std.Io.Reader) !Torrent {
-    var value = try parseValue(alloc, reader);
+    var value = try parseValue(alloc, reader, 0);
     errdefer value.deinit(alloc);
 
-    const dict = value.dict;
+    const dict = value.inner.dict;
 
     return Torrent{
         .value = value,
-        .announce = if (dict.get("announce")) |v| v.string else return error.MissingAnnounce,
+        .announce = if (dict.get("announce")) |v| v.inner.string else return error.MissingAnnounce,
         .announceList = if (dict.get("announce-list")) |v| blk: {
-            const list = v.list.items[0].list;
+            const list = v.inner.list.items[0].inner.list;
 
             const ret = try alloc.alloc([]const u8, list.items.len);
             errdefer alloc.free(ret);
 
-            for (list.items, 0..) |item, i| ret[i] = item.string;
+            for (list.items, 0..) |item, i| ret[i] = item.inner.string;
 
             break :blk ret;
         } else null,
         .created_by = null,
         .creation_date = null,
-        .info = if (dict.get("info")) |v| v.dict else return error.MissingInfo,
+        .info = if (dict.get("info")) |v| v.inner.dict else return error.MissingInfo,
     };
 }
 
@@ -288,4 +339,17 @@ test "parseTorrent - simple" {
 
     var torrent = try parseTorrent(std.testing.allocator, &reader);
     defer torrent.deinit(std.testing.allocator);
+}
+
+test "parseTorrent - info hash" {
+    const file = @embedFile("./testing.torrent");
+    var reader: std.Io.Reader = .fixed(file);
+
+    var torrent = try parseTorrent(std.testing.allocator, &reader);
+    defer torrent.deinit(std.testing.allocator);
+
+    reader = .fixed(file);
+
+    const hash = try torrent.computeInfoHash(&reader);
+    std.debug.print("{s}", .{std.fmt.bytesToHex(hash, .lower)});
 }
