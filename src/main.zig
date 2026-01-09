c1105479f5725ac326faa572564c52e718a5a489
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2026-01-09 21:05:35 +0200

    refactor(main): allow providing custom torrent files through args

--- src/main.zig
+++ src/main.zig
@@ -4,13 +4,21 @@ const tozi = @import("tozi");
 pub fn main() !void {
     // var arena: std.heap.ArenaAllocator = .init(std.heap.smp_allocator);
     // defer arena.deinit();
+    // const alloc = arena.allocator();
 
     var gpa: std.heap.DebugAllocator(.{}) = .init;
     defer _ = gpa.deinit();
-
     const alloc = gpa.allocator();
 
-    const torrentPath = "src/test_files/copper.torrent";
+    const args = try std.process.argsAlloc(alloc);
+    defer std.process.argsFree(alloc, args);
+
+    if (args.len < 2) {
+        std.log.err("provide path to torrent as first arg", .{});
+        return;
+    }
+
+    const torrentPath = args[1];
     const file = std.fs.cwd().openFile(torrentPath, .{}) catch {
         std.log.err("failed openning {s} file", .{torrentPath});
         return;
